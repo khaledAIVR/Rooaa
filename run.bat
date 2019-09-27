88be920e640f8f08c4a02a9269a9a55dcf1b517a
Abdelrahman Kandil <abdelrahmankandil@hotmail.com>
2019-09-27 21:10:14 +0200

    Added options to run on WSL or Windows

--- run.bat
+++ run.bat
@@ -1,4 +1,13 @@
-start bash -c -i "cd redis-stable && src/redis-server"
+REM Run on WSL 
+
+REM start bash -c -i "cd redis-stable && src/redis-server"
+REM timeout 5
+REM start bash -c -i "source linux-venv/bin/activate && celery worker -A manage:celery -l info -f celery.log"
+REM start bash -c -i "source linux-venv/bin/activate && python manage.py"
+
+REM Run on Windows instead..
+
+start CMD /K bash -c -i "cd redis-stable && src/redis-server"
 timeout 5
-start bash -c -i "source linux-venv/bin/activate && celery worker -A manage.celery --loglevel=info"
-start bash -c -i "source linux-venv/bin/activate && python manage.py"
+start CMD /K "celery worker -A manage:celery -l info -f celery.log"
+python manage.py
