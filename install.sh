3393e8a01e15533cef97add805b7d9ef15e8c17a
Abdelrahman Kandil <abdelrahmankandil@hotmail.com>
2019-10-31 21:38:26 +0200

    Added docker support

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Installing redis
-if [ ! -d redis-stable/src ]; then
-    curl -O http://download.redis.io/redis-stable.tar.gz
-    tar xvzf redis-stable.tar.gz
-    rm redis-stable.tar.gz
-fi
-cd redis-stable
-make
-
-# Installing pip requirements
-cd ..
-pip install -r requirements.txt
-
 # Installing darknet model
 cd rooaa/ml-model/darknet
 wget https://pjreddie.com/media/files/yolov3.weights
